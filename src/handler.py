59b6ad53db8de3c391847a9fa016a4ab473d2078
Trevor Facer <trevorf@ifit.com>
2019-07-14 03:21:28 -0600

    Add create route

--- src/handler.py
+++ src/handler.py
@@ -1,7 +1,78 @@
+import boto3
 import json
 
 
+# # For a Boto3 client.
+# table = ddb.Table('BucketItems')
+
+# res = table.put_item(
+#    Item={
+#         'name': 'First Item',
+#         'url': 'www.example.com',
+#         'Id': 'test'
+#     }
+# )
+
+
+def create_bucket_item(event, context):
+    print(f"event: {event}")
+    print(f"event.body: {event['body']}")
+    # ddb = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
+    ddb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
+    table = ddb.Table('BucketItems')
+    # body = event['body']
+    #body = event['body']
+    # body = json.parevent['body']
+    print('before body')
+    body = json.loads(event['body'])
+    print('after body')
+    # print(type(body))
+    # body = json.loads(event['body'])
+    print(f"body: {body}")
+    name = body['name']
+    print('after name')
+    url = body['url']
+    my_id = body['id']
+    if not body or not url or  not id:
+        print(f"body didn't contain necessary fields: name, url, id")
+    item = {
+        'name': name,
+        'url': url,
+        'Id': my_id
+    }
+    print(f"item: {item}")
+    res = table.put_item(Item=item)
+    print(f"res: {res}")
+    res_body = {
+        "message": "Put item",
+    }
+    code = res["ResponseMetadata"]['HTTPStatusCode']
+
+    response = {
+        "statusCode": code,
+        "body": json.dumps(res_body)
+    }
+
+    return response
+    # response = client.put_item(
+    #     TableName='BucketItems',
+    #     Item={
+    #         'string': {
+    #             'S': 'string',
+    #             'N': 'string',
+    #             'B': b'bytes',
+    #             'SS': [
+    #                 'string',
+    #             ],
+
+
+
+
 def get_one_bucket_item(event, context):
+    ddb = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
+    response = ddb.list_tables()
+    print(response)
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
